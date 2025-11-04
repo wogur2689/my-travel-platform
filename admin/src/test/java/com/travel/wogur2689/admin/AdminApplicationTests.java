03c28ce9ab6086e7abda6c2a7f4cef70ccf0a672
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2025-11-05 02:30:23 +0900

    feat : 최초생성

---
+++ admin/src/test/java/com/travel/wogur2689/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.wogur2689.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user/src/test/java/com/travel/wogur2689/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.wogur2689.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
