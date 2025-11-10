5a666cf4ef29cf77e91b5618e352673187058313
wogur2689 <anfvnxntm@naver.com>
2025-11-10 17:40:24 +0900

    fix : package 경로 생성

--- user/src/main/java/com/travel/wogur2689/user/pub/PageController.java
+++ user/src/main/java/com/travel/wogur2689/user/pub/PageController.java
@@ -1,8 +1,10 @@
+package com.travel.wogur2689.user.pub;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class PageController {
 
     @GetMapping("/")
     public String mainPage() {
